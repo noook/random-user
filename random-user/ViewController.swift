fed16c7070d7960b7473c6657f630cb31a8e8fc5
neilrichter <me@neilrichter.com>
2019-01-22 14:21:48 +0100

    Initial Commit

---
+++ random-user/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  random-user
+//
+//  Created by Neil Richter on 22/01/2019.
+//  Copyright © 2019 Neil Richter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
